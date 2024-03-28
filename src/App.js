4e036385af7d5ad5aa397ad48397a0dc5ad80608
Djordje Savanovic <ds21546@essex.ac.uk>
2024-03-28 14:33:44 +0100

    chore: add initial styles - Implemented Bootstrap & first custom styles

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container-fluid bg-primary">
+			<div className="row">
+				<div className={'d-flex flex-column col-12 align-items-center justify-content-center vh-100'}>
+					<img src={logo} className="App-logo" alt="logo"/>
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
