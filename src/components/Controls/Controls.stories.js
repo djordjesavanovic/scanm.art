cf5b7983ac4200559089b14b5d3b1547e72757bb
Djordje Savanovic <ds21546@essex.ac.uk>
2024-04-02 12:27:38 +0200

    chore: final-touch-up - Added bootstrap css to the stories

--- src/components/Controls/Controls.stories.js
+++ src/components/Controls/Controls.stories.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Controls from './Controls';
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 export default {
   title: 'Example/Controls',
